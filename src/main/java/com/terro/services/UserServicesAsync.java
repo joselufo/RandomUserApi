c8eb1f1113ab511c2456c8b411b73d3607bf9483
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2014-08-28 09:44:32 +0200

    Added method of UserServicesAsync

--- src/main/java/com/terro/services/UserServicesAsync.java
+++ src/main/java/com/terro/services/UserServicesAsync.java
@@ -1,7 +1,46 @@
 package com.terro.services;
 
-/**
- * Created by TeRRo on 28/08/14.
- */
-public class UserServicesAsync {
+import com.terro.entities.UserRandomResponse;
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
+public interface UserServicesAsync {
+
+    /**
+     * Returns a random user asynchronously.
+     *
+     * @param callback
+     */
+    @GET("/")
+    public void userAsync(Callback<UserRandomResponse> callback);
+
+    /**
+     * Returns a list of random users asynchronously.
+     *
+     * @param results - Number of users
+     * @param callback - Callback
+     */
+    @GET("/")
+    public void listUserAsync(@Query("results") Integer results, Callback<UserRandomResponse> callback);
+
+    /**
+     * Returns a random user with a specific gender asynchronously.
+     *
+     * @param gender - Gender user to return (Male or Fermale)
+     * @param callback - Callback
+     */
+    @GET("/")
+    public void userWithGenderAsync(@Query("gender") String gender, Callback<UserRandomResponse> callback);
+
+    /**
+     * Returns a list of random users with a specific gender asynchronously.
+     *
+     * @param results - Number of users
+     * @param gender - Gender user to return (Male or Female)
+     * @param callback - Callback
+     */
+    @GET("/")
+    public void listUsersWithGenderAsync(@Query("results") Integer results, @Query("gender") String gender, Callback<UserRandomResponse> callback);
+
 }
