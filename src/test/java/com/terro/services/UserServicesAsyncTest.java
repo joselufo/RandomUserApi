6d1cc22c227e972fcb0699cf7dd6cf419bfecc0a
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2014-08-28 10:02:56 +0200

    Added UserServicesAsyncTest class.

---
+++ src/test/java/com/terro/services/UserServicesAsyncTest.java
@@ -0,0 +1,22 @@
+package com.terro.services;
+
+import junit.framework.TestCase;
+
+public class UserServicesAsyncTest extends TestCase {
+
+    public void testUserAsync() throws Exception {
+
+    }
+
+    public void testListUserAsync() throws Exception {
+
+    }
+
+    public void testUserWithGenderAsync() throws Exception {
+
+    }
+
+    public void testListUsersWithGenderAsync() throws Exception {
+
+    }
+}
--- src/test/java/com/terro/services/UserServicesTest.java
+++ src/test/java/com/terro/services/UserServicesTest.java
@@ -1,7 +1,11 @@
-package com.terro;
+package com.terro.services;
 
 
+import com.terro.BaseTestCase;
 import com.terro.entities.UserRandomResponse;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
@@ -16,6 +20,18 @@ public class UserServicesTest extends BaseTestCase {
     }
 
     public void testUserAsynchronous() throws Exception {
+
+        getManager().userServicesAsync().userAsync(new Callback<UserRandomResponse>() {
+            @Override
+            public void success(UserRandomResponse userRandomResponse, Response response) {
+                assertThat(userRandomResponse).isNotNull();
+            }
+
+            @Override
+            public void failure(RetrofitError retrofitError) {
+
+            }
+        });
 
     }
 
