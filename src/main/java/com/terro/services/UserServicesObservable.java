8684ee35d81f3fbdb593ffb700d2335df2fadb6b
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2014-08-28 17:52:49 +0200

    Implemented all method of class UserServicesObservable

--- src/main/java/com/terro/services/UserServicesObservable.java
+++ src/main/java/com/terro/services/UserServicesObservable.java
@@ -1,7 +1,26 @@
 package com.terro.services;
 
-/**
- * Created by TeRRo on 28/08/14.
- */
+
+import com.terro.entities.UserRandomResponse;
+import retrofit.http.GET;
+import retrofit.http.Query;
+import rx.Observable;
+
 public interface UserServicesObservable {
+
+    @GET("/")
+    public Observable<UserRandomResponse> user();
+
+    @GET("/")
+    public Observable<UserRandomResponse> listUsers(@Query("results") Integer results);
+
+    @GET("/")
+    public Observable<UserRandomResponse> userWithGender(@Query("gender") String gender);
+
+    @GET("/")
+    public Observable<UserRandomResponse> listUsersWithGender(@Query("results") Integer results, @Query("gender") String gender);
+
+    @GET("/")
+    public Observable<UserRandomResponse> userWithSeed (@Query("seed") String seed);
+
 }
