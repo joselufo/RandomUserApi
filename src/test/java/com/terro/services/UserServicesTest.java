402fbbd3955a74e3f4bbc6f50e02831ebfb36053
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2014-08-28 10:30:40 +0200

    Implementation test for the class UserServicesTest

--- src/test/java/com/terro/services/UserServicesTest.java
+++ src/test/java/com/terro/services/UserServicesTest.java
@@ -2,10 +2,10 @@ package com.terro.services;
 
 
 import com.terro.BaseTestCase;
+import com.terro.Gender;
+import com.terro.entities.Result;
+import com.terro.entities.User;
 import com.terro.entities.UserRandomResponse;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
@@ -15,19 +15,46 @@ public class UserServicesTest extends BaseTestCase {
 
         UserRandomResponse result = getManager().userServices().user();
         assertThat(result).isNotNull();
-
+        assertThat(result.getResults().size()).isEqualTo(1);
 
     }
 
     public void testListUsers() throws Exception {
-
+        UserRandomResponse response = getManager().userServices().listUsers(20);
+        assertThat(response).isNotNull();
+        assertThat(response.getResults().size()).isEqualTo(20);
     }
 
     public void testUserWithGender() throws Exception {
+        UserRandomResponse responseMale = getManager().userServices().userWithGender(Gender.MALE);
+        assertThat(responseMale).isNotNull();
+        assertThat(responseMale.getResults().get(0).getUser().getGender()).isEqualTo(Gender.MALE);
 
+        UserRandomResponse responseFemale = getManager().userServices().userWithGender(Gender.FEMALE);
+        assertThat(responseFemale).isNotNull();
+        assertThat(responseFemale.getResults().get(0).getUser().getGender()).isEqualTo(Gender.FEMALE);
     }
 
     public void testListUsersWithGender() throws Exception {
+        UserRandomResponse response = getManager().userServices().listUsersWithGender(20, Gender.MALE);
+        assertThat(response).isNotNull();
+        assertThat(response.getResults().size()).isEqualTo(20);
+
+        for (Result result : response.getResults()) {
+            User user = result.getUser();
+            assertThat(user).isNotNull();
+            assertThat(user.getGender()).isEqualTo(Gender.MALE);
+        }
+
+        UserRandomResponse responseFemale = getManager().userServices().listUsersWithGender(20, Gender.FEMALE);
+        assertThat(responseFemale).isNotNull();
+        assertThat(responseFemale.getResults().size()).isEqualTo(20);
+
+        for (Result resultFemale : responseFemale.getResults()) {
+            User user = resultFemale.getUser();
+            assertThat(user).isNotNull();
+            assertThat(user.getGender()).isEqualTo(Gender.FEMALE);
+        }
 
     }
 
