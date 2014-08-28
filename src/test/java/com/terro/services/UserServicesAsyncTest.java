78b68791b9f1218cd9d386c6810bbe12b8a4b056
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2014-08-28 10:53:00 +0200

    Implementation test for the UserServicesAsyncTest class.

--- src/test/java/com/terro/services/UserServicesAsyncTest.java
+++ src/test/java/com/terro/services/UserServicesAsyncTest.java
@@ -2,20 +2,30 @@ package com.terro.services;
 
 import com.terro.BaseTestCase;
 import com.terro.Gender;
+import com.terro.entities.Result;
+import com.terro.entities.User;
 import com.terro.entities.UserRandomResponse;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class UserServicesAsyncTest extends BaseTestCase {
+
+    private CountDownLatch lock = new CountDownLatch(1);
+    private UserRandomResponse responseUserRandom;
+
 
     public void testUserAsync() throws Exception {
         getManager().userServicesAsync().userAsync(new Callback<UserRandomResponse>() {
             @Override
             public void success(UserRandomResponse userRandomResponse, Response response) {
-                assertThat(userRandomResponse).isNotNull();
+                responseUserRandom = userRandomResponse;
+                lock.countDown();
             }
 
             @Override
@@ -23,13 +33,18 @@ public class UserServicesAsyncTest extends BaseTestCase {
 
             }
         });
+
+        lockAwait();
+        assertThat(responseUserRandom).isNotNull();
+        assertThat(responseUserRandom.getResults().size()).isEqualTo(1);
     }
 
     public void testListUserAsync() throws Exception {
         getManager().userServicesAsync().listUserAsync(20, new Callback<UserRandomResponse>() {
             @Override
             public void success(UserRandomResponse userRandomResponse, Response response) {
-                assertThat(userRandomResponse).isNotNull();
+                responseUserRandom = userRandomResponse;
+                lock.countDown();
             }
 
             @Override
@@ -37,13 +52,18 @@ public class UserServicesAsyncTest extends BaseTestCase {
 
             }
         });
+
+        lockAwait();
+        assertThat(responseUserRandom).isNotNull();
+        assertThat(responseUserRandom.getResults().size()).isEqualTo(20);
     }
 
     public void testUserWithGenderAsync() throws Exception {
-        getManager().userServicesAsync().userWithGenderAsync(Gender.MALE, new Callback<UserRandomResponse>() {
+        getManager().userServicesAsync().userWithGenderAsync(Gender.FEMALE, new Callback<UserRandomResponse>() {
             @Override
             public void success(UserRandomResponse userRandomResponse, Response response) {
-                assertThat(userRandomResponse).isNotNull();
+                responseUserRandom = userRandomResponse;
+                lock.countDown();
             }
 
             @Override
@@ -51,13 +71,18 @@ public class UserServicesAsyncTest extends BaseTestCase {
 
             }
         });
+
+        lockAwait();
+        assertThat(this.responseUserRandom).isNotNull();
+        assertThat(this.responseUserRandom.getResults().get(0).getUser().getGender()).isEqualTo(Gender.FEMALE);
     }
 
     public void testListUsersWithGenderAsync() throws Exception {
         getManager().userServicesAsync().listUsersWithGenderAsync(20, Gender.FEMALE, new Callback<UserRandomResponse>() {
             @Override
             public void success(UserRandomResponse userRandomResponse, Response response) {
-                assertThat(userRandomResponse).isNotNull();
+                responseUserRandom = userRandomResponse;
+                lock.countDown();
             }
 
             @Override
@@ -65,5 +90,23 @@ public class UserServicesAsyncTest extends BaseTestCase {
 
             }
         });
+
+        lockAwait();
+        assertThat(responseUserRandom).isNotNull();
+        assertThat(responseUserRandom.getResults().size()).isEqualTo(20);
+
+        for (Result resultFemale : responseUserRandom.getResults()) {
+            User user = resultFemale.getUser();
+            assertThat(user).isNotNull();
+            assertThat(user.getGender()).isEqualTo(Gender.FEMALE);
+        }
+    }
+
+    private void lockAwait () {
+        try {
+            lock.await(10000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
